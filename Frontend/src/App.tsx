02c924f48ead7a43151f4624129cd3994e97b2fe
dobidugi <dobidugi@gmail.com>
2022-12-08 11:39:03 +0900

    Feat:  add Main, Login Page Component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,9 @@ import * as React from "react";
 import Header from "./components/header/Header";
 import GlobalStyle from "./GlobalStyle";
 import { Routes, Route, Navigate } from "react-router-dom";
+
+import Main from "./pages/Main/Index";
+
 function App() {
     return (
         <div
@@ -11,14 +14,15 @@ function App() {
         >
             <GlobalStyle />
             <Header />
-            <Routes>
-                <Route path="/" element={<Navigate to="/main" />} />
-                <Route path="/main" element={<div>Home</div>} />
-                <Route path="/funding" element={<div>funding</div>} />
-                <Route path="/commingsoon" element={<div>commingsoon</div>} />
-            </Routes>
-
-
+            <div className="content">
+                <Routes>
+                    <Route path="/" element={<Navigate to="/main" />} />
+                    <Route path="/main" element={<Main />} />
+                    <Route path="/login" element={<div>login</div>} />
+                    <Route path="/funding" element={<div>funding</div>} />
+                    <Route path="/commingsoon" element={<div>commingsoon</div>} />
+                </Routes>
+            </div>
         </div>
     );
 }
@@ -26,6 +30,12 @@ function App() {
 const defaultStyle = css`
     width: 100%;
     min-height: 100vh;
+
+    @media screen and (max-width: 1095px) {
+        & > .content {
+            padding-top: 4rem;
+        }
+    }
 `;
 
 export default App;
