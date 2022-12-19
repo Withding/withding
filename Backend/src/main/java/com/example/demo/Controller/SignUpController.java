d852a6d25bf574a93060e03acbe01789887416ba
AntenA <80020067+ycs-202007055@users.noreply.github.com>
2022-12-19 23:51:46 +0900

    Feat: 회원가입 이메일 전송 구현

--- Backend/src/main/java/com/example/demo/Controller/SignUpController.java
+++ Backend/src/main/java/com/example/demo/Controller/SignUpController.java
@@ -1,6 +1,7 @@
 package com.example.demo.Controller;
 
 import com.example.demo.DTO.User;
+import com.example.demo.Repository.EmailAuthRepo;
 import com.example.demo.Service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,13 +19,35 @@ public class SignUpController {
     @Autowired
     private MailService mailService;
 
-    @RequestMapping(value = "/user", method = RequestMethod.POST)
-    public ResponseEntity<Object> SignUp(@RequestBody final User request){
-
-        mailService.sendSignUpCode(request.getEmail());
+    @Autowired
+    private EmailAuthRepo emailAuthRepo;
 
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    private final int DIGIT = 6;        // 이메일 인증 자릿수
+
+    /**
+     * 회원가입 전용 이메일 발송 함수
+     * @param request email을 담을 User 객체
+     * @return 정상 처리시 HttpStatus.NO_CONTENT, 비정상 처리시 HttpStatus.BAD_REQUEST
+     */
+    @RequestMapping(value = "/user/auth/email", method = RequestMethod.GET)
+    public ResponseEntity<Object> getEmailAuth(@RequestBody final User request){
+        final String CODE = mailService.getEmailAuthCode(DIGIT);                        // 코드 생성
+        if (mailService.sendSignUpCode(request.getEmail(), CODE)){                      // 메일로 해당 코드 전송
+            emailAuthRepo.save(request.getEmail(), CODE);                               // emailauth 테이블에 이메일, 코드 저장
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
+
+    @RequestMapping(value = "/user/auth/check", method = RequestMethod.POST)
+    public ResponseEntity<Object> authEmailCode(@RequestBody final User request) {
+
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
 
 }
--- Backend/src/main/java/com/example/demo/Service/MailService.java
+++ Backend/src/main/java/com/example/demo/Service/MailService.java
@@ -23,27 +23,41 @@ public class MailService {
      * 회원가입 전용 이메일 발송
      * @param email 발송할 대상
      */
-    public void sendSignUpCode(String email) {
-        //System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
+    public boolean sendSignUpCode(final String email, final String CODE) {
         System.setProperty("mail.smtp.ssl.protocols", "TLSv1.2");
-        System.setProperty("mail.pop3s.ssl.protocols", "TLSv1.2");
-
-        String str = String.valueOf((int)(Math.random() * 1000000) % 999999);
+        boolean result = false;
 
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
-        String htmlMsg = "<p> 해당 코드를 입력해주세요. </p> <br> <p>" + str + "</p>";                    // 메일 내용에 삽입될 부분
+        String htmlMsg = "<p> 해당 코드를 입력해주세요. </p> <br> <p>" + CODE + "</p>";                    // 메일 내용에 삽입될 부분
         //mimeMessage.setContent(htmlMsg, "text/html"); /** Use this or below line **/
         try {
             helper.setTo(email); // 받는 사람
             helper.setSubject("Withding 가입 안내 코드입니다."); // 메일 제목
             helper.setText(htmlMsg, true); // 메일 내용
             helper.setFrom("amusicmeet@gmail.com"); // 보내는 사람
+            javaMailSender.send(mimeMessage);
+            result = true;
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
+        return result;
+    }
 
-        javaMailSender.send(mimeMessage);
+
+    /**
+     * Stirng 타입의 랜덤 숫자 코드를 생성하는 함수
+     * @param digit 자릿수
+     * @return String 타입의 숫자
+     */
+    public String getEmailAuthCode(int digit){
+        char[] strs = {'0','1','2','3','4','5','6','7','8','9'};
+        String code = "";
+        // Math.random() * ( 최대값 - 최소값 )  ) + 최소값
+        for(int i = 0; i < digit; i++){
+            code = code + strs[(int)(Math.random() * (strs.length - 0) + 0)];
+        }
+        return code;
     }
 
 
