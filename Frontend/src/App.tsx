6ebc87b5377c6097a6d35545b378cea1d6777f94
dobidugi <dobidugi@gmail.com>
2022-12-20 13:46:37 +0900

    Feat: Email에 인증코드 전송 API 테스트전, queryClinet 추가

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,41 +5,54 @@ import GlobalStyle from "./GlobalStyle";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 const Main = React.lazy(() => import("./pages/Main/Index"));
-// import Main from "./pages/Main/Index";
 const Signin = React.lazy(() => import("./pages/Signin/Index"));
 const Signup = React.lazy(() => import("./pages/Signup/Signup"));
-// import Signup from "./pages/Signup/Signup";
 const Oauth = React.lazy(() => import("./pages/Oauth/Index"));
-// import Oauth from "./pages/Oauth/Index";
+
 import { Suspense } from "react";
 import Auth from "./components/auth/Auth";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            suspense: true,
+            useErrorBoundary: true,
+            retry: 0,
+            refetchOnWindowFocus: false,
+
+        },
+    },
+});
 
 function App() {
     return (
-        <Auth>
-            <div
-                className="container"
-                css={defaultStyle}
-            >
-                <GlobalStyle />
-                <Header />
-                <div className="content">
-                    <Suspense fallback={<div>loading</div>}>
-                        <Routes>
-                            <Route path="/" element={<Navigate to="/main" />} />
-                            <Route path="/main" element={<Main />} />
-                            <Route path="/signin/*" element={<Signin />} />
-                            <Route path="/signup/*" element={<Signup />} />
-                            <Route path="/oauth/*" element={<Oauth />} />
-                            <Route path="/funding" element={<div>funding</div>} />
-                            <Route path="/commingsoon" element={<div>commingsoon</div>} />
-                        </Routes>
-                    </Suspense>
+        <QueryClientProvider client={queryClient}>
+            <Auth>
+                <div
+                    className="container"
+                    css={defaultStyle}
+                >
+                    <GlobalStyle />
+                    <Header />
+                    <div className="content">
+                        <Suspense fallback={<div>loading</div>}>
+                            <Routes>
+                                <Route path="/" element={<Navigate to="/main" />} />
+                                <Route path="/main" element={<Main />} />
+                                <Route path="/signin/*" element={<Signin />} />
+                                <Route path="/signup/*" element={<Signup />} />
+                                <Route path="/oauth/*" element={<Oauth />} />
+                                <Route path="/funding" element={<div>funding</div>} />
+                                <Route path="/commingsoon" element={<div>commingsoon</div>} />
+                            </Routes>
+                        </Suspense>
 
+                    </div>
                 </div>
-            </div>
-        </Auth>
+            </Auth >
+        </QueryClientProvider>
     );
 }
 
--- Frontend/src/pages/Signup/EmailSignup/EmailSignup.tsx
+++ Frontend/src/pages/Signup/EmailSignup/EmailSignup.tsx
@@ -1,15 +1,17 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback } from "react";
 import EmailSignupContext from "../../../store/EmailSignupContext";
 import EmailSignupForm from "./EmailSignupForm";
 import useForm from "../../../hooks/use-form";
 import SignupValidator from "./SignupValidator";
+import { useQuery } from "react-query";
+import requestAuthCode from "@/utils/RequestApis/signup/requestAuthCode";
 
 /**
  * 이메일 회원가입 컴포넌트
  * @returns 
  */
 function EmailSignup() {
-    const [isSuccessSendMail, setIsSuccessSendMail] = useState<boolean>(false); // 이메일에 인증코드 전송여부
+    // const [isSuccessSendMail, setIsSuccessSendMail] = useState<boolean>(false); // 이메일에 인증코드 전송여부
     const { values, onChangeValues, errors } = useForm({
         initValues: {
             email: "",
@@ -20,12 +22,19 @@ function EmailSignup() {
         validator: SignupValidator
     });
 
+    // 이메일에 인증코드 보내기
+    const { refetch: requestCode, isSuccess: isSuccessSendMail } =
+        useQuery(["requestAuthCode"], () => requestAuthCode(values.email), {
+            enabled: false,
+        });
 
+    // 이메일 인증번호 클릭
     const sendEmailHandler = useCallback(() => {
         console.log(values.email);
-        setIsSuccessSendMail(true);
-    }, [values.email]);
+        requestCode();
+    }, [requestCode, values.email]);
 
+    // 회원가입 버튼 클릭
     const onSubmit = useCallback((event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         console.log("submit");
