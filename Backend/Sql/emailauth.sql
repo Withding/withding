0bbdba5c9eeb7056bff75b42108ceab48ff9c6c0
AntenA <80020067+ycs-202007055@users.noreply.github.com>
2022-12-22 00:49:27 +0900

    Docs: 회원가입_코드인증.md 작성 및 관련 Sql파일 수정

--- Backend/Sql/emailauth.sql
+++ Backend/Sql/emailauth.sql
@@ -2,5 +2,6 @@ CREATE TABLE `emailauth` (
 	`emailauth_id` INT(5) auto_increment NOT NULL PRIMARY KEY,
 	`email` VARCHAR(100) NOT NULL,
 	`code` VARCHAR(50) NOT NULL,
+	`secretkey` VARCHAR(200) NOT NULL,
 	`deadline` TimeStamp NOT NULL
 );
