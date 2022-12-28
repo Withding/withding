e7a8584708f40ccdcfcdc934bf991e874611bb51
AntenA <80020067+ycs-202007055@users.noreply.github.com>
2022-12-28 22:55:03 +0900

    Refactor: 카카오 로그인 Response 객체를 User -> ResponseLogin으로 변경

--- Backend/src/main/java/com/example/demo/Controller/LoginController.java
+++ Backend/src/main/java/com/example/demo/Controller/LoginController.java
@@ -6,19 +6,22 @@ import com.example.demo.DTO.User;
 import com.example.demo.Service.JwtService;
 import com.example.demo.Service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
 @Controller
-@CrossOrigin("*")
+@CrossOrigin("http://localhost:3000/")
 public class LoginController {
 
     @Autowired
@@ -37,11 +40,10 @@ public class LoginController {
     /**
      * 카카오 로그인
      * @param request 카카오 API에서 받은 엑세스 토큰 (json)
-     * @param servletResponse 쿠키를 담을 리스폰스
      * @return 닉네임, 스테이터스 코드
      */
     @RequestMapping(value = "/auth/kakao", method = RequestMethod.POST)
-    public ResponseEntity<Object> kakaoauth(@RequestBody final User request, HttpServletResponse servletResponse){
+    public ResponseEntity<Object> kakaoauth(@RequestBody final User request){
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         User user = loginService.kakaoLogin(request.getAccessToken());
 
@@ -50,20 +52,11 @@ public class LoginController {
         }
         else {
             String jwt = jwtService.generateJwtToken(user.getUserId(), user.getNickName(), dateFormat.format(new Timestamp(System.currentTimeMillis())));
-
-            Cookie cookie = new Cookie("jwt",jwt); // create a cookie
-            cookie.setMaxAge(7 * 24 * 60 * 60); // expires in 7 days
-            // optional properties
-            //cookie.setSecure(true);
-            cookie.setHttpOnly(true);
-            cookie.setPath("/");
-
-            servletResponse.addCookie(cookie); // add cookie to response
-
-            return new ResponseEntity<>(user, HttpStatus.CREATED);
+            return new ResponseEntity(new ResponseLogin(jwt, user.getNickName()), HttpStatus.OK);
         }
 
     }
+
 
 
     /**
