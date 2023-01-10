3a5563fe839afedebf8e4eff0939a3dcc4caf20a
AntenA <80020067+ycs-202007055@users.noreply.github.com>
2023-01-10 19:06:19 +0900

    Refactor: SecurityConfig의 인터셉터 접근 범위 수정
    /user/mypage/**

--- Backend/src/main/java/com/example/demo/Config/SecurityConfig.java
+++ Backend/src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package com.example.demo.Config;
 
+import com.example.demo.Service.IntercepterService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -17,6 +20,14 @@ public class SecurityConfig {
                 .cors().disable()
                 .headers().frameOptions().disable();
         return http.build();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new IntercepterService())
+                .addPathPatterns("/user/mypage/**"); // 해당 경로에 접근하기 전에 인터셉터가 가로챈다.
+        //      .excludePathPatterns("/user/image/**"); // 해당 경로는 인터셉터가 가로채지 않는다.
+        WebMvcConfigurer.super.addInterceptors(registry);
     }
 
 }
