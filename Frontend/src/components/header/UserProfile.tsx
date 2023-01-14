d95a28d087e04d22ed4a4932dd158d14ba2e6503
dobidugi <dobidugi@gmail.com>
2023-01-14 21:24:46 +0900

    Feat: 헤더 닉네임 클릭시 mypage로 이동

--- Frontend/src/components/header/UserProfile.tsx
+++ Frontend/src/components/header/UserProfile.tsx
@@ -1,5 +1,6 @@
 import UserContext from "@/store/UserContext";
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 /**
  * 헤더에 들어갈 유저 프로필 컴포넌트
@@ -7,9 +8,10 @@ import React, { useContext } from "react";
  */
 function UserProfile() {
     const { nickName } = useContext(UserContext);
+    const navigate = useNavigate();
     return (
         <div>
-            <p>{nickName}</p>
+            <p onClick={() => navigate("/mypage/main")}>{nickName}</p>
         </div>
     );
 }
