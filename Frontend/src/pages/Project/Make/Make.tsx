614876fb16fb98454c2a43430224437b760b9f75
dobidugi <dobidugi@gmail.com>
2023-01-18 22:20:21 +0900

    Design: 프로젝트 생성시 진행단계 설명해주는 페이지 구현

--- Frontend/src/pages/Project/Make/Make.tsx
+++ Frontend/src/pages/Project/Make/Make.tsx
@@ -5,11 +5,13 @@ import React, { useCallback, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Intro from "./Intro/Intro";
 import Ready from "./Ready/Ready";
+import Button from "@/components/common/Button";
 
 
 interface EpisodeType {
     step: number;
     component: EmotionJSX.Element;
+    nextButtonValue: string;
 }
 
 /**
@@ -22,15 +24,21 @@ function Make() {
     const step = parseInt(new URLSearchParams(search).get("step") ?? "1");
     const navigator = useNavigate();
     const episode: EpisodeType[] = [
-        { step: 1, component: <Intro /> },
-        { step: 2, component: <Ready /> },
+        { step: 1, component: <Intro />, nextButtonValue: "좋아요" },
+        { step: 2, component: <Ready />, nextButtonValue: "다음" },
+        { step: 3, component: <Ready />, nextButtonValue: "다음" },
     ];
-    const render = episode.find((item) => item.step === step)?.component; // step에 해당하는 컴포넌트를 렌더링  
+    const render = episode.find((item) => item.step === step);// step에 해당하는 컴포넌트를 렌더링  
 
     const goNextStepHandler = useCallback(() => {
-        navigator(`/project/make?step=${step + 1}`);
-    }, [navigator, step]);
+        if (episode.length > step)
+            navigator(`/project/make?step=${step + 1}`);
+    }, [episode.length, navigator, step]);
 
+    const prevStepHandler = useCallback(() => {
+        if (step !== 1)
+            navigator(`/project/make?step=${step - 1}`);
+    }, [navigator, step]);
     return (
         <ProjectMakeContext.Provider value={{
             goNextStepHandler
@@ -40,7 +48,23 @@ function Make() {
                     menu
                 </aside>
                 <main>
-                    {render}
+                    {render?.component}
+                    <article className="button">
+                        {render?.step !== 1 &&
+                            <Button
+                                className="prev"
+                                onClick={prevStepHandler}
+                                value="< 이전"
+                            />
+                        }
+                        {episode.length > render!.step &&
+                            <Button
+                                className="next"
+                                onClick={goNextStepHandler}
+                                value={render?.nextButtonValue ?? ""}
+                            />
+                        }
+                    </article>
                 </main>
             </div>
         </ProjectMakeContext.Provider>
@@ -53,13 +77,14 @@ const style = css`
     height: 100%;
     display: inline-flex;
     line-height: 1.3;
+
     h1{
         font-weight: 800;
         font-size: 2.5rem;
         margin-bottom: 1rem;
     }
 
-    p {
+    p[className="description"] {
         font-size: 1.1rem;
         color: var(--grey-500);
         font-weight: 400;
@@ -70,9 +95,41 @@ const style = css`
         padding: 1rem 2rem;
     }
 
+    .button {
+        margin-top: 2rem;
+        width: 100%;
+    }
+
+    button {
+        min-height: 48px;
+        text-align: center;
+        margin-right: 2rem;
+    }
+
+    .next {
+        color: var(--white);
+        background-color: var(--green-300);
+        min-width: 15rem;
+    }
+
+    .next:hover {
+        background-color: var(--green-400);
+    }
+
+    .prev {
+        color: var(--grey-500);
+    }
+
     @media screen and (max-width: 1095px) {
         aside {
             display: none;
+        }
+
+        .next {
+            width: 20%;
+        }
+        .prev {
+            width: 20%;
         }
     }
 `;
---
+++ Frontend/src/pages/Project/Make/Ready/Ready.tsx
@@ -0,0 +1,50 @@
+import ProcedureType from "@/types/ProcedureType";
+import { css } from "@emotion/react";
+import React from "react";
+import ProcedureList from "@/components/common/ProcedureList";
+import { IoAlertCircle } from "react-icons/io5";
+
+/**
+ * /project/make?step=2 페이지 컴포넌트
+ * 프로젝트 생성시 진행 단계를 보여주는 페이지
+ * @returns 
+ */
+function Ready() {
+    const procedures: ProcedureType[] = [
+        { step: 1, title: "1단계: 프로젝트 생성", description: "필수 항목들을 작성해 제출하시면 됩니다." },
+        { step: 2, title: "2단계:프로젝트 검토", description: "작성한 내용들을 어떤식으로 게시되는지 미리 보고 검토할 수 있답니다." },
+        { step: 3, title: "3단계:프로젝트 공개", description: "모든 준비가 끝난 프로젝트를 언제 공개할지 메이커님의 최종 승인이 필요해요." },
+    ];
+    return (
+        <article css={style}>
+            <h1>{`프로젝트는 총 ${procedures.length}단계로 생성돼요`}</h1>
+            <p className="description">멋진 프로젝트를 완성시키기 위해 필요한 단계들이랍니다.</p>
+            <section>
+                <span><IoAlertCircle /><p>모든 단계를 거쳐야 성공적으로 등록할 수 있어요</p></span>
+            </section>
+            <ProcedureList
+                list={procedures}
+            />
+        </article>
+    );
+}
+
+const style = css`
+    max-width: 600px;
+    section {
+        margin-top: 1rem;
+        font-size: 0.8rem;
+        color: var(--grey-500);
+        span {
+            display: inline-flex;
+        }
+        svg {
+            font-size: 1.1rem;
+            margin-right: 0.5rem;
+        }
+        padding: 1rem;
+        background-color: var(--grey-100);
+    }
+`;
+
+export default Ready;
