5e7eb9a814a8d28d75a2b1b0bc28fcfacb25de93
AntenA <80020067+ycs-202007055@users.noreply.github.com>
2023-01-22 23:18:10 +0900

    Refactor: FileController -> imageService() 수정
    유저 프로필 이미지만 담당하던 것에서 모든 이미지 처리를 담당하도록 수정

--- Backend/src/main/java/com/example/demo/Config/BeanConfig.java
+++ Backend/src/main/java/com/example/demo/Config/BeanConfig.java
@@ -22,7 +22,7 @@ public class BeanConfig {
     public final String PROFILE_IMAGE_URL = "/user/image/";                                                             // 유저 프로필 호출 URL
     public final String PROFILE_IMAGE_PATH = System.getProperty("user.dir") + File.separator + "profileImages/";        // 유저 이미지 파일 저장 경로
 
-    public final String THUMBNAIL_IMAGE_URL = "";
+    public final String THUMBNAIL_IMAGE_URL = "/thumbnail/image";
 
     public final String THUMBNAIL_IMAGE_PATH = System.getProperty("user.dir") + File.separator + "thumbnailImages/";
 
