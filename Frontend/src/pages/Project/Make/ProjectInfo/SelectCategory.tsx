8358627341dae13b7ec8b03fe1221429c02177b5
dobidugi <dobidugi@gmail.com>
2023-01-23 18:56:18 +0900

    Rename: Category -> SelectCategory

---
+++ Frontend/src/pages/Project/Make/ProjectInfo/SelectCategory.tsx
@@ -0,0 +1,43 @@
+import Category from "@/types/Category";
+import fetchCategorys from "@/utils/RequestApis/category/fetchCategorys";
+import { css } from "@emotion/react";
+import React from "react";
+import { useQuery } from "react-query";
+
+/**
+ * 프로젝트 카테고리 선택 컴포넌트
+ * @returns 
+ */
+function SelectCategory() {
+    const { data } = useQuery(["makeCategory"], () => fetchCategorys(), {
+        suspense: false,
+        useErrorBoundary: false
+    });
+    return (
+        <section>
+            <label>
+                <span>카테고리</span>
+                <select
+                    css={style}
+                >
+                    <option value="0">카테고리</option>
+                    {data?.categoryList.map((item: Category) => (
+                        <option key={item.id} value={item.id}>{item.category}</option>
+                    ))}
+                </select>
+            </label>
+        </section>
+    );
+}
+
+const style = css`
+    all: unset;
+    margin-top: 0.5rem;
+    appearance: button;
+    padding: 0 1rem;
+    border: 1px solid var(--grey-200);
+    background-color: unset;
+    min-height: 48px;
+`;
+
+export default SelectCategory;
