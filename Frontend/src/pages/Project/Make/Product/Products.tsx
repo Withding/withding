3d25816dc8ff2620bb58a1d729364d8fa55097b5
dobidugi <dobidugi@gmail.com>
2023-02-01 21:32:55 +0900

    Feat: 프로젝트 생성시 해당 프로젝트 상품 리스트 가져오는 AP

--- Frontend/src/pages/Project/Make/Product/Products.tsx
+++ Frontend/src/pages/Project/Make/Product/Products.tsx
@@ -1,12 +1,18 @@
 import AlertBox from "@/components/common/AlertBox";
 import React from "react";
 import ProductInputForm from "./ProductInputForm";
+import { useQuery } from "react-query";
+import useProjectParam from "@/hooks/useProjectParam";
+import fetchProductList from "@/utils/RequestApis/projectmake/fetchProductList";
 
 /**
  * 프로젝트 생성시 물건 등록하는 컴포넌트
  * @returns 
  */
 function Products() {
+    const project = useProjectParam();
+    const { data } = useQuery(["makeProductList", project], () => fetchProductList(project));
+    console.log(data);
     return (
         <article>
             <h1>상품 등록</h1>
