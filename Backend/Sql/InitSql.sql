2f8323ae690a8b5d394c0e4ecbf5c3b32bf46da4
AntenA <80020067+ycs-202007055@users.noreply.github.com>
2023-02-13 22:55:24 +0900

    Docs: thumbnail 테이블에 기본 이미지 추가

--- Backend/Sql/InitSql.sql
+++ Backend/Sql/InitSql.sql
@@ -109,6 +109,7 @@ CREATE TABLE `thumbnail` (
 	`image` VARCHAR(100) NOT NULL PRIMARY KEY,
 	`origin_image` VARCHAR(100) NOT NULL
 );
+INSERT INTO `thumbnail` VALUES('default.jpeg', 'default.jpeg');
 
 
 CREATE TABLE `funding` (
--- Backend/Sql/thumbnail.sql
+++ Backend/Sql/thumbnail.sql
@@ -2,3 +2,5 @@ CREATE TABLE `thumbnail` (
 	`image` VARCHAR(100) NOT NULL PRIMARY KEY,
 	`origin_image` VARCHAR(100) NOT NULL
 );
+
+INSERT INTO `thumbnail` VALUES('default.jpeg', 'default.jpeg');
