c237021d3d56916ce69fc33686c7751b21570840
dobidugi <dobidugi@gmail.com>
2023-02-15 22:59:31 +0900

    Feat: 마이페이지 메이커 정보  서버로부터 받아오기

--- Frontend/src/pages/MyPage/Main/Main.tsx
+++ Frontend/src/pages/MyPage/Main/Main.tsx
@@ -31,7 +31,6 @@ const style = css`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
     .content {
         width: 100%;
     }
--- Frontend/src/pages/MyPage/Main/Maker/MakerContext.tsx
+++ Frontend/src/pages/MyPage/Main/Maker/MakerContext.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
+import Logout from "../Logout";
+import Profile from "../Profile/Profile";
+import { useQuery } from "react-query";
+import fetchMakerInfo from "@/utils/RequestApis/mypage/fetchMakerInfo";
 
 /**
  * 메이커 탭 클릭시 보여주는 화면
  * @returns 
  */
 function MakerContext() {
+    const isMobile = useMediaQuery({ query: "(max-width: 1095px)" });
+    const { data } = useQuery(["fetchMakerInfo"], () => fetchMakerInfo(), {
+        suspense: false,
+        useErrorBoundary: false
+    });
     return (
         <React.Fragment>
-            123
+            <Profile
+                isEditProfileImage={false}
+                type={1}
+                followerCount={data?.followerCount}
+            />
+            {isMobile && <Logout />}
         </React.Fragment>
     );
 }
