2581bb2bcc88da429253d91754ffcbb56e13d2b8
AntenA <80020067+ycs-202007055@users.noreply.github.com>
2023-02-21 00:01:35 +0900

    Docs: point_history.sql 오타 수정, 내가 작성한 프로젝트 목록 호출.md 수정

--- Backend/Sql/point_history.sql
+++ Backend/Sql/point_history.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `point_ history` (
+CREATE TABLE `point_history` (
 	`point_history_id` INT(10) auto_increment NOT NULL PRIMARY KEY,
 	`user_id` INT(10) NOT NULL,
 	`point` BIGINT(1) NOT NULL,
@@ -7,14 +7,14 @@ CREATE TABLE `point_ history` (
 	`source` VARCHAR(50) NOT NULL
 );
 
-ALTER TABLE `point_ history` ADD CONSTRAINT `FK_user_TO_point_ history_1` FOREIGN KEY (
+ALTER TABLE `point_history` ADD CONSTRAINT `FK_user_TO_point_history_1` FOREIGN KEY (
 	`user_id`
 )
 REFERENCES `user` (
 	`user_id`
 );
 
-ALTER TABLE `point_ history` ADD CONSTRAINT `FK_point_type_TO_point_ history_1` FOREIGN KEY (
+ALTER TABLE `point_history` ADD CONSTRAINT `FK_point_type_TO_point_history_1` FOREIGN KEY (
 	`type_code`
 )
 REFERENCES `point_type` (
