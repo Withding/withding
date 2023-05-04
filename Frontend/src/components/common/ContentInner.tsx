aed50d60ce3a8344bb0d98727b62405a6fbb22a1
dobidugi <dobidugi@gmail.com>
2023-05-04 22:51:29 +0900

    Feat: /common/ContentInner 컴포넌트 구현
    max-width, min-width를 가지고있는 inner 컴포는트 구현

---
+++ Frontend/src/components/common/ContentInner.tsx
@@ -0,0 +1,30 @@
+import { css } from "@emotion/react";
+import React from "react";
+
+/**
+ * pc버전 max-width가 정해져있는 div
+ * @returns 
+ */
+function ContentInner(props: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div css={style} className=".inner">{props.children}</div>
+    );
+}
+
+const style = css`
+    width: 100%;
+    @media screen and (min-width: 1096px) {
+        max-width: var(--pc-max-width);
+        min-width: var(--pc-max-width);
+    }
+
+    @media screen and (max-width: 1095px) {
+        display: inline-flex;
+        justify-content: center;
+    }
+
+`;
+
+export default ContentInner;
