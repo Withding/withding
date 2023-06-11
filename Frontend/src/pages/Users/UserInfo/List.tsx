051df52d9f179b2a9c6f678b4d791dc1b8859c61
dobidugi <dobidugi@gmail.com>
2023-06-11 23:02:51 +0900

    Feat: 유저 펀딩 리스트 화면에 뿌려주기

--- Frontend/src/pages/Users/UserInfo/List.tsx
+++ Frontend/src/pages/Users/UserInfo/List.tsx
@@ -1,11 +1,35 @@
+import InfinityScroll from "@/components/common/InfinityScroll";
+import UserFundingItem from "@/components/common/UserFunding/UserFundingItem";
 import UserFundingList from "@/components/common/UserFunding/UserFundingList";
+import useUserInfoContext from "@/hooks/useUserInfoContext";
 import React from "react";
 
 function List() {
+    const { list } = useUserInfoContext();
+    console.log(list.fundingList);
     return (
-        <UserFundingList>
+        <InfinityScroll
+            hasNextPage={list.hasNextPage}
+            fetchNextPage={list.fetchNextPage}
+            isFetchingNextPage={list.isFetchingNextPage}
+        >
+            <UserFundingList>
+                {
+                    list.fundingList?.map((item) => (
+                        <UserFundingItem
+                            key={item.id}
+                            title={item.title}
+                            imgSrc={item.image}
+                            alt={item.id.toString()}
+                            url={`/project/make?project=${item.id}`}
+                            state={item.state}
+                            underText={"상세 정보"}
+                        />
+                    ))
+                }
+            </UserFundingList>
+        </InfinityScroll>
 
-        </UserFundingList>
     );
 }
 
