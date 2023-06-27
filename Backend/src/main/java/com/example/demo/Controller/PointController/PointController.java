4dc4aa8b0050a20b145e30f3232889919f83c084
AntenA <80020067+ycs-202007055@users.noreply.github.com>
2023-06-27 23:11:19 +0900

    Refactor: 포인트 url 수정된 부분에 맞춰 로직 수정

--- Backend/src/main/java/com/example/demo/Controller/PointController/PointController.java
+++ Backend/src/main/java/com/example/demo/Controller/PointController/PointController.java
@@ -2,16 +2,12 @@ package com.example.demo.Controller.PointController;
 
 import com.example.demo.DTO.User;
 import com.example.demo.Service.PointService;
-import com.example.demo.Service.ProjectService;
 import com.example.demo.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -27,20 +23,22 @@ public class PointController {
 
     /**
      * 이벤트로 포인트 지급
-     * @param point 지급할 포인트
+     *
      * @param request request userNum, nickName, loginTime이 속성으로 들어있는 HttpServletRequest 객체
-     * @return
+     * @param point 지급할 포인트
+     * @return 정상 = 204, 실패 = 400, 인증실패 = 401
      */
-    @RequestMapping(value = "/point", method = RequestMethod.POST)
-    public ResponseEntity<Object> chargePoint(@RequestParam Long point,
-                                              HttpServletRequest request) {
-        // ------------------------------ 인증 --------------------------------------------------------------------------
-        User user = userService.setUserToHttpServletRequestAttribute(request);
-        if (user == null) {
+    @PostMapping(value = "/users/{userId}/point/{point}")
+    public ResponseEntity<Object> chargePoint(HttpServletRequest request,
+                                              @PathVariable("userId") Long userId,
+                                              @PathVariable("point") Long point) {
+        // ------------------------------ 인증(이 부분 바꿔야됨 일반 유저가 아닌 관리자의 토큰이 필요) --------------------------------------------------------------------------
+        User admin = userService.setUserToHttpServletRequestAttribute(request);
+        if (admin == null) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
         // -------------------------------------------------------------------------------------------------------------
-        if (pointService.chargePoint(user, point, "회원가입 이벤트")){
+        if (pointService.chargePoint(userId, point, "회원가입 이벤트")){
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
--- Backend/src/main/java/com/example/demo/Service/PointService.java
+++ Backend/src/main/java/com/example/demo/Service/PointService.java
@@ -25,17 +25,18 @@ public class PointService {
 
     /**
      * 포인트 충전하는 함수 충전시킬 user와 충천할 포인트를 인수로 받는다
-     * @param user 충전 대상
+     * @param userId 충전 대상의 userId
      * @param point 충전 포인트
      * @return 정상 = true, 비정상 = false
      */
-    public boolean chargePoint(User user, Long point, String source){
+    public boolean chargePoint(Long userId, Long point, String source){
         EntityManager em = JpaConfig.emf.createEntityManager();
         EntityTransaction tr = em.getTransaction();
 
         try{
             tr.begin();
-            user = em.find(User.class, user.getUserId());
+
+            User user = em.find(User.class, userId);
             user.setPoint(user.getPoint() + point);
 
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
