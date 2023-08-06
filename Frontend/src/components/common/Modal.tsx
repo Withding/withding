297eb57031b858b94487a2ae2cc7371d2d8430c7
dobidugi <dobidugi@gmail.com>
2023-08-06 18:36:33 +0900

    Fix: Modal css 수정

--- Frontend/src/components/common/Modal.tsx
+++ Frontend/src/components/common/Modal.tsx
@@ -59,9 +59,15 @@ function Modal(props:
 
 const backdropStyle = css`
     position: fixed;
-    width: 100%;
-    height: 100%;
+    left: 0;
+    top: 0;
+    right: 0;
+    width: 100vw;
+    height: 100vh;
+
     background-color: rgba(0, 0, 0, 0.75);
+    backdrop-filter: blur(1px);
+    z-index: 999;
 `;
 
 export default Modal;
