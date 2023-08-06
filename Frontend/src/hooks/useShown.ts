8980745278c46c33274a9eddf7bcc327cc34c01d
dobidugi <dobidugi@gmail.com>
2023-08-06 18:37:02 +0900

    Feat: Modal 열림여부, 닫기, 열기를 관리하는 hooks 구현

--- Frontend/src/hooks/useShown.ts
+++ Frontend/src/hooks/useShown.ts
@@ -2,8 +2,9 @@ import { useState } from "react";
 
 function useShown(defaultValue = false) {
     const [isShowing, setIsShowing] = useState(defaultValue);
-    const onToggle = () => setIsShowing(!isShowing);
-    return { isShowing, onToggle };
+    const onClose = () => setIsShowing(() => false);
+    const onOpen = () => setIsShowing(() => true);
+    return { isShowing, onClose, onOpen };
 }
 
 export default useShown;
