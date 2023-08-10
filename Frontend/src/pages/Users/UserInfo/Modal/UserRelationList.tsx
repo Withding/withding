e0dcacb0ae81ad1226833bb588f4f0157aef01f4
dobidugi <dobidugi@gmail.com>
2023-08-10 21:12:35 +0900

    Rename: UserRelaction -< UserRelation

--- Frontend/src/pages/Users/UserInfo/Modal/UserRelationList.tsx
+++ Frontend/src/pages/Users/UserInfo/Modal/UserRelationList.tsx
@@ -1,50 +1,10 @@
 import React from "react";
-import UserRelactionItem from "./UserRelactionItem";
+import UserRelationItem from "./UserRelationItem";
 
 function UserRelationList() {
     return (
         <ul>
-            <UserRelactionItem
-                name="이름"
-                img="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                relation={true}
-            />
-            <UserRelactionItem
-                name="이름"
-                img="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                relation={true}
-            />
-            <UserRelactionItem
-                name="이름"
-                img="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                relation={true}
-            />
-            <UserRelactionItem
-                name="이름"
-                img="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                relation={true}
-            />
-            <UserRelactionItem
-                name="이름"
-                img="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                relation={true}
-            />
-            <UserRelactionItem
-                name="이름"
-                img="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                relation={true}
-            />
-            <UserRelactionItem
-                name="이름"
-                img="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                relation={true}
-            />
-            <UserRelactionItem
-                name="이름"
-                img="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                relation={true}
-            />
-            <UserRelactionItem
+            <UserRelationItem
                 name="이름"
                 img="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
                 relation={true}
